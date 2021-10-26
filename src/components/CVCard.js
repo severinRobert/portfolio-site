3eb93133d2f69c6143eebf854e3a19c516a7599d
severinRobert <64019051+severinRobert@users.noreply.github.com>
2021-10-26 15:58:42 +0200

    Add CVCard components

---
+++ src/components/CVCard.js
@@ -0,0 +1,9 @@
+function CVCard(props) {
+  return (
+    <div className="card">
+      <h1>{props.title}</h1>
+    </div>
+  );
+}
+
+export default CVCard;
--- src/pages/CV.js
+++ src/pages/CV.js
@@ -1,7 +1,18 @@
+import CVCard from '../components/CVCard.js';
+
 function CV() {
   return (
     <div className="cv">
-      <h1>CV page</h1>
+      <div className="row">
+        <div className="column">
+          <CVCard title="profil"/>
+          <CVCard title="skills"/>
+        </div>
+        <div className="column">
+          <CVCard title="experiences"/>
+          <CVCard title="formations"/>
+        </div>
+      </div>
     </div>
   );
 }
